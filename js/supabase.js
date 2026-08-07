995d4249f2a55babd42b35c96cca6172d82f31ae
hameedsbistro <prokashlaptop@gmail.com>
2026-08-08 05:28:01 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,18 +1,70 @@
-// pos/js/supabase.js
-
-import { createClient } from 
-"https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+// js/supabase.js
 
 
-const supabaseUrl = 
+import {
+
+createClient
+
+} from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+
+
+
+
+// ==========================
+// SUPABASE CONFIG
+// ==========================
+
+
+// আপনার Supabase Project URL এখানে দিন
+
+const SUPABASE_URL =
 "https://wrabxqejkabbheddioxa.supabase.co";
 
 
-const supabaseKey = 
+
+
+// আপনার Supabase anon public key এখানে দিন
+
+const SUPABASE_ANON_KEY =
+
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyYWJ4cWVqa2FiYmhlZGRpb3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMzUwODAsImV4cCI6MjEwMTYxMTA4MH0.ss-HG38k8ZeEzV5Icg55_G7Pkz4VuY7AbbxWAnCJpLk";
 
 
-export const supabase = createClient(
-    supabaseUrl,
-    supabaseKey
+
+
+
+
+
+// ==========================
+// CREATE CLIENT
+// ==========================
+
+
+export const supabase =
+
+createClient(
+
+SUPABASE_URL,
+
+SUPABASE_ANON_KEY,
+
+{
+
+auth:{
+
+
+persistSession:true,
+
+
+autoRefreshToken:true,
+
+
+detectSessionInUrl:true
+
+
+}
+
+}
+
 );
